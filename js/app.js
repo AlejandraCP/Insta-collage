87868b80e8147b3b7d9872941be691b68fda0cdf
AlejandraCP <alejandracp710@gmail.com>
2018-02-07 16:24:26 -0500

    Agregando data

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var photosBox = document.querySelector('.photos-box');
 (function() {
-  var i = 1;
-  for (i; i < 13; i++) {
+  var i = 0;
+  for (i; i < photos.length; i++) {
     var containerPhoto = document.createElement('div');
     containerPhoto.classList.add('cont-photo');
     containerPhoto.classList.add('col-md-2');
     containerPhoto.classList.add('col-sm-4');
     var photo = document.createElement('img');
-    photo.src = '../assets/images/naturaleza' + i + '.jpg';
+    photo.src = '../assets/images/' + photos[i];
     photo.setAttribute('draggable', true);
     containerPhoto.appendChild(photo);
     photosBox.appendChild(containerPhoto);
---
+++ js/data.js
@@ -0,0 +1,14 @@
+photos = [
+  'naturaleza1.jpg',
+  'naturaleza2.jpg',
+  'naturaleza3.jpg',
+  'naturaleza4.jpg',
+  'naturaleza5.jpg',
+  'naturaleza6.jpg',
+  'naturaleza7.jpg',
+  'naturaleza8.jpg',
+  'naturaleza9.jpg',
+  'naturaleza10.jpg',
+  'naturaleza11.jpg',
+  'naturaleza12.jpg'
+];
