38a66958ae2d5dc4531888564372c46b11bc9193
AlejandraCP <alejandracp710@gmail.com>
2018-02-07 16:13:07 -0500

    Agregando atributo graggable

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@ var photosBox = document.querySelector('.photos-box');
   var i = 1;
   for (i; i < 13; i++) {
     var containerPhoto = document.createElement('div');
-    containerPhoto.classList.add('cont-Photo');
+    containerPhoto.classList.add('cont-photo');
     containerPhoto.classList.add('col-md-2');
-    containerPhoto.classList.add('col-sm-3');
+    containerPhoto.classList.add('col-sm-4');
     var photo = document.createElement('img');
     photo.src = '../assets/images/naturaleza' + i + '.jpg';
+    photo.setAttribute('draggable', true);
     containerPhoto.appendChild(photo);
     photosBox.appendChild(containerPhoto);
     photo.classList.add('img-fluid');
