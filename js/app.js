7312ed205a28a51ffa1e3f5343f629261b7fd23b
AlejandraCP <alejandracp710@gmail.com>
2018-02-07 21:47:23 -0500

    collage con 4 imágenes

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var photosBox = document.querySelector('.photos-box');
     var photo = document.createElement('img');
     photo.src = '../assets/images/' + photos[i] + '.jpg';
     photo.setAttribute('draggable', true);
-    photo.setAttribute('id', 'photo' + photos[i]);
+    photo.setAttribute('id', photos[i]);
     containerPhoto.appendChild(photo);
     photosBox.appendChild(containerPhoto);
     photo.classList.add('img-fluid');
@@ -31,7 +31,7 @@ function permitirDrop(event) {
 
 function drop(event) {
   event.preventDefault();
-  if (event.target.id === 'photo1' || event.target.id === 'photo2' || event.target.id === 'photo3') {
+  if (event.target.id === 'photo1' || event.target.id === 'photo2' || event.target.id === 'photo3' || event.target.id === 'photo4') {
     let idFoto = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById(idFoto));
     console.log(event.target);
@@ -45,4 +45,44 @@ function end(event) {
   event.target.classList.add('img-size');
 }
 
-// opciones de collage
+// opciones de collage para 4 fotos
+
+var photo4 = document.getElementById('photo4');
+var photo5 = document.getElementById('photo5');
+
+photo4.addEventListener('click', collage4);
+
+var big = document.querySelector('.big');
+function collage4() {
+  var parent = document.getElementById('parent');
+  parent.parentNode.removeChild(parent);
+  var contain1 = document.createElement('div');
+  contain1.classList.add('row');
+  contain1.setAttribute('id', 'parent');
+  var firstChild = document.createElement('div');
+  firstChild.classList.add('col-6', 'pad', 'collage');
+  firstChild.setAttribute('id', 'photo1');
+  var seconChild = document.createElement('div');
+  seconChild.classList.add('col-6', 'pad', 'collage');
+  seconChild.setAttribute('id', 'photo2');
+
+  
+  var contain2 = document.createElement('div');
+  contain2.classList.add('row');
+  contain2.setAttribute('id', 'parent2');
+  var thirdChild = document.createElement('div');
+  thirdChild.classList.add('col-6', 'pad', 'collage');
+  thirdChild.setAttribute('id', 'photo3');
+  var fourthChild = document.createElement('div');
+  fourthChild.classList.add('col-6', 'pad', 'collage');
+  fourthChild.setAttribute('id', 'photo4');
+  
+  big.appendChild(contain1);
+  contain1.appendChild(firstChild);
+  contain1.appendChild(seconChild);
+
+  big.appendChild(contain2);
+  contain2.appendChild(thirdChild);
+  contain2.appendChild(fourthChild);
+
+}
